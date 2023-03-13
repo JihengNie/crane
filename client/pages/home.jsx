9fce626b717e06e2b751dc0fdb6be1b8032ad85c
Jiheng Nie <Niejiheng@gmail.com>
2023-03-13 23:09:58 +0000

    Started on vote page

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../components/header.jsx';
+import Header from '../components/header';
 import Carousel from 'react-bootstrap/Carousel';
 
 export default function Home(props) {
@@ -7,40 +7,32 @@ export default function Home(props) {
     <div>
       <Header />
       <Carousel variant="dark">
-        <Carousel.Item className='carousel-item-settings'>
-          <img
+        <Carousel.Item>
+          <div className='carousel-item-settings'>
+            <img
             className="d-block w-100"
             src="/img/crane/c1.png"
             alt="First slide"
-          />
-          <Carousel.Caption>
-            <h5>Crane 1</h5>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
+            />
+          </div>
         </Carousel.Item>
-        <Carousel.Item className='carousel-item-settings'>
-          <img
+        <Carousel.Item >
+          <div className='carousel-item-settings'>
+            <img
             className="d-block w-100"
             src="/img/crane/c2.png"
             alt="Second slide"
           />
-          <Carousel.Caption>
-            <h5>Crane 2</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
+          </div>
         </Carousel.Item>
-        <Carousel.Item className='carousel-item-settings'>
-          <img
+        <Carousel.Item>
+          <div className='carousel-item-settings'>
+            <img
             className="d-block w-100"
             src="/img/placeholder.png"
             alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h5>More cranes in the future</h5>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
+            />
+          </div>
         </Carousel.Item>
       </Carousel>
     </div>
